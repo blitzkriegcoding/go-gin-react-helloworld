ffba2401581c2940464a5ce325e393c594e707e1
Christopher Konopka <cskonopka@schwwaaa.net>
2018-06-20 19:01:27 -0400

    changed repo name to go-gin-react-helloworld

--- views/js/app.jsx
+++ views/js/app.jsx
@@ -4,8 +4,9 @@ class HelloWorld extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
-            <h1>go-gin-react</h1>
+            <h1>go-gin-react-helloworld</h1>
             <h2>Hello World!</h2>
+            <h3>Hello World web app created with Golang + Gin + React</h3>
             <h4><a href="https://github.com/cskonopka/go-gin-react">Github</a></h4>
           </div>
         </div>
